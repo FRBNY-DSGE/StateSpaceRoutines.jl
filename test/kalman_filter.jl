ce4039fa925caa6234783f5765829799eb53fbe2
Pearl Li <pearlzli16@gmail.com>
2017-02-17 15:17:57 -0500

    Fix Kalman filter test again

--- test/kalman_filter.jl
+++ test/kalman_filter.jl
@@ -15,16 +15,14 @@ out = kalman_filter(data, TTT, RRR, CCC, QQ, ZZ, DD, MM, EE, z0, P0)
 
 h5 = h5open("$path/reference/kalman_filter_out.h5")
 @test_approx_eq read(h5, "L")             out[1]
-@test_approx_eq read(h5, "zend")          out[2]
-@test_approx_eq read(h5, "Pend")          out[3]
-@test_approx_eq read(h5, "pred")          out[4]
-@test_approx_eq read(h5, "vpred")         out[5]
-@test_approx_eq read(h5, "yprederror")    out[6]
-@test_approx_eq read(h5, "ystdprederror") out[7]
-@test_approx_eq read(h5, "rmse")          out[8]
-@test_approx_eq read(h5, "rmsd")          out[9]
-@test_approx_eq read(h5, "filt")          out[10]
-@test_approx_eq read(h5, "vfilt")         out[11]
+@test_approx_eq read(h5, "pred")          out[2]
+@test_approx_eq read(h5, "vpred")         out[3]
+@test_approx_eq read(h5, "yprederror")    out[4]
+@test_approx_eq read(h5, "ystdprederror") out[5]
+@test_approx_eq read(h5, "rmse")          out[6]
+@test_approx_eq read(h5, "rmsd")          out[7]
+@test_approx_eq read(h5, "filt")          out[8]
+@test_approx_eq read(h5, "vfilt")         out[9]
 close(h5)
 
 # Method with initial conditions omitted
@@ -34,16 +32,14 @@ out = kalman_filter(data, TTT, RRR, CCC, QQ, ZZ, DD, MM, EE)
 # we allow greater ϵ
 h5 = h5open("$path/reference/kalman_filter_out.h5")
 @test_approx_eq     read(h5, "L")             out[1]
-@test_approx_eq     read(h5, "zend")          out[2]
-@test_approx_eq_eps read(h5, "Pend")          out[3]  1e-1
-@test_approx_eq     read(h5, "pred")          out[4]
-@test_approx_eq_eps read(h5, "vpred")         out[5]  1e-1
-@test_approx_eq     read(h5, "yprederror")    out[6]
-@test_approx_eq     read(h5, "ystdprederror") out[7]
-@test_approx_eq     read(h5, "rmse")          out[8]
-@test_approx_eq     read(h5, "rmsd")          out[9]
-@test_approx_eq     read(h5, "filt")          out[10]
-@test_approx_eq_eps read(h5, "vfilt")         out[11] 1e-1
+@test_approx_eq     read(h5, "pred")          out[2]
+@test_approx_eq_eps read(h5, "vpred")         out[3]  1e-1
+@test_approx_eq     read(h5, "yprederror")    out[4]
+@test_approx_eq     read(h5, "ystdprederror") out[5]
+@test_approx_eq     read(h5, "rmse")          out[6]
+@test_approx_eq     read(h5, "rmsd")          out[7]
+@test_approx_eq     read(h5, "filt")          out[8]
+@test_approx_eq_eps read(h5, "vfilt")         out[9] 1e-1
 close(h5)
 
 nothing
