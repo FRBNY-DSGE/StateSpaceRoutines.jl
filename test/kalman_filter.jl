e1749ab4b9cc92c089af5642216a6c5a5663ff5a
Pearl Li <pearlzli16@gmail.com>
2017-02-17 15:45:05 -0500

    Use 10 periods of data for Kalman filter test

--- test/kalman_filter.jl
+++ test/kalman_filter.jl
@@ -4,7 +4,7 @@ using HDF5
 path = dirname(@__FILE__)
 
 # Initialize arguments to function
-h5 = h5open("$path/reference/kalman_filter_args.h5")
+h5 = h5open("$path/reference/kalman_filter_args.h5", "r")
 for arg in ["data", "TTT", "RRR", "CCC", "QQ", "ZZ", "DD", "MM", "EE", "z0", "P0"]
     eval(parse("$arg = read(h5, \"$arg\")"))
 end
@@ -13,16 +13,16 @@ close(h5)
 # Method with all arguments provided
 out = kalman_filter(data, TTT, RRR, CCC, QQ, ZZ, DD, MM, EE, z0, P0)
 
-h5 = h5open("$path/reference/kalman_filter_out.h5")
-@test_approx_eq read(h5, "L")             out[1]
-@test_approx_eq read(h5, "pred")          out[2]
-@test_approx_eq read(h5, "vpred")         out[3]
-@test_approx_eq read(h5, "yprederror")    out[4]
-@test_approx_eq read(h5, "ystdprederror") out[5]
-@test_approx_eq read(h5, "rmse")          out[6]
-@test_approx_eq read(h5, "rmsd")          out[7]
-@test_approx_eq read(h5, "filt")          out[8]
-@test_approx_eq read(h5, "vfilt")         out[9]
+h5 = h5open("$path/reference/kalman_filter_out.h5", "r")
+@test_approx_eq read(h5, "log_likelihood") out[1]
+@test_approx_eq read(h5, "pred")           out[2]
+@test_approx_eq read(h5, "vpred")          out[3]
+@test_approx_eq read(h5, "yprederror")     out[4]
+@test_approx_eq read(h5, "ystdprederror")  out[5]
+@test_approx_eq read(h5, "rmse")           out[6]
+@test_approx_eq read(h5, "rmsd")           out[7]
+@test_approx_eq read(h5, "filt")           out[8]
+@test_approx_eq read(h5, "vfilt")          out[9]
 close(h5)
 
 # Method with initial conditions omitted
@@ -30,16 +30,16 @@ out = kalman_filter(data, TTT, RRR, CCC, QQ, ZZ, DD, MM, EE)
 
 # Pend, vpred, and vfilt matrix entries are especially large, averaging 1e5, so
 # we allow greater ϵ
-h5 = h5open("$path/reference/kalman_filter_out.h5")
-@test_approx_eq     read(h5, "L")             out[1]
-@test_approx_eq     read(h5, "pred")          out[2]
-@test_approx_eq_eps read(h5, "vpred")         out[3]  1e-1
-@test_approx_eq     read(h5, "yprederror")    out[4]
-@test_approx_eq     read(h5, "ystdprederror") out[5]
-@test_approx_eq     read(h5, "rmse")          out[6]
-@test_approx_eq     read(h5, "rmsd")          out[7]
-@test_approx_eq     read(h5, "filt")          out[8]
-@test_approx_eq_eps read(h5, "vfilt")         out[9] 1e-1
+h5 = h5open("$path/reference/kalman_filter_out.h5", "r")
+@test_approx_eq     read(h5, "log_likelihood") out[1]
+@test_approx_eq     read(h5, "pred")           out[2]
+@test_approx_eq_eps read(h5, "vpred")          out[3]  1e-1
+@test_approx_eq     read(h5, "yprederror")     out[4]
+@test_approx_eq     read(h5, "ystdprederror")  out[5]
+@test_approx_eq     read(h5, "rmse")           out[6]
+@test_approx_eq     read(h5, "rmsd")           out[7]
+@test_approx_eq     read(h5, "filt")           out[8]
+@test_approx_eq_eps read(h5, "vfilt")          out[9] 1e-1
 close(h5)
 
 nothing
