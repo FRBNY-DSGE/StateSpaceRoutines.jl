60fc14a679b632baed1d3f49947f36dc6efdd3ab
Abhi Gupta <abhig94@gmail.com>
2017-10-03 16:03:05 -0400

    Refactor away DSGE dependence in tpf test

--- test/tempered_particle_filter.jl
+++ test/tempered_particle_filter.jl
@@ -1,21 +1,13 @@
-m = AnSchorfheide()
-df = readtable("/data/dsge_data_dir/dsgejl/mcai/TPF/save/input_data/data/us.txt", header=false, separator=' ')
-data = convert(Matrix{Float64}, df)'
+using Base.Test
 
-params = [2.09, 0.98, 2.25, 0.65, 0.34, 3.16, 0.51, 0.81, 0.98, 0.93, 0.19, 0.65, 0.24,
-          0.115985, 0.294166, 0.447587]
+path = dirname(@__FILE__)
 
-update!(m, params)
+args_file = h5open("$path/reference/tempered_particle_filter_args.h5")
+for arg in ["data", "TTT", "RRR", "QQ", "ZZ", "DD", "HH", "data", "s0", "s_init"]
+    eval(parse("$arg = read(args_file, \"$arg\")"))
+end
+close(args_file)
 
-system  = compute_system(m)
-
-s0 = zeros(n_states_augmented(m))
-RRR     = system[:RRR]
-TTT     = system[:TTT]
-HH      = system[:EE]
-DD      = system[:DD]
-ZZ      = system[:ZZ]
-QQ      = system[:QQ]
 sqrtS2 = RRR*chol(QQ)'
 
 Φ(s_t::Vector{Float64}, ϵ_t::Vector{Float64}) = TTT*s_t + sqrtS2*ϵ_t
@@ -24,7 +16,6 @@ sqrtS2 = RRR*chol(QQ)'
 F_ϵ = Distributions.MvNormal(zeros(size(QQ, 1)), eye(size(QQ, 1)))
 F_u = Distributions.MvNormal(zeros(size(HH, 1)), HH)
 
-s_init = h5read("reference/tempered_particle_filter_args.h5", "s_init")
 fixed_sched = [0.2, 0.5, 1.0]
 
 test_data = reshape(data[:, 1], 3, 1)
@@ -34,7 +25,8 @@ loglik, lik, _ = tempered_particle_filter(test_data, Φ, Ψ, F_ϵ, F_u, s_init; 
                          adaptive = false, fixed_sched = fixed_sched, allout = true, parallel = false,
                          testing = true)
 
-file = h5open("reference/tempered_particle_filter_out.h5", "r")
+
+file = h5open("$path/reference/tempered_particle_filter_out.h5", "r")
 test_loglik = read(file, "log_lik")
 test_lik    = read(file, "incr_lik")
 close(file)
