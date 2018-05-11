0bffed6ce4f0e159312652d33f739a205a8ac37e
Pearl Li <pearlzli16@gmail.com>
2018-05-11 16:50:21 -0400

    Rename Kalman filter test variables

--- test/kalman_filter.jl
+++ test/kalman_filter.jl
@@ -1,14 +1,15 @@
 path = dirname(@__FILE__)
 
 # Initialize arguments to function
-h5 = h5open("$path/reference/kalman_filter_args.h5", "r")
-for arg in ["data", "TTT", "RRR", "CCC", "QQ", "ZZ", "DD", "EE", "z0", "P0"]
-    eval(parse("$arg = read(h5, \"$arg\")"))
-end
-close(h5)
+file = h5open("$path/reference/kalman_filter_args.h5", "r")
+y = read(file, "data")
+T, R, C    = read(file, "TTT"), read(file, "RRR"), read(file, "CCC")
+Q, Z, D, E = read(file, "QQ"), read(file, "ZZ"), read(file, "DD"), read(file, "EE")
+s_0, P_0   = read(file, "z0"), read(file, "P0")
+close(file)
 
 # Method with all arguments provided
-out = kalman_filter(data, TTT, RRR, CCC, QQ, ZZ, DD, EE, z0, P0)
+out = kalman_filter(y, T, R, C, Q, Z, D, E, s_0, P_0)
 
 h5open("$path/reference/kalman_filter_out.h5", "r") do h5
     @test read(h5, "log_likelihood") ≈ sum(out.loglh)
@@ -17,12 +18,12 @@ h5open("$path/reference/kalman_filter_out.h5", "r") do h5
     @test read(h5, "vpred")          ≈ out.P_pred
     @test read(h5, "filt")           ≈ out.s_filt
     @test read(h5, "vfilt")          ≈ out.P_filt
-    @test z0                         ≈ out.s_0
-    @test P0                         ≈ out.P_0
+    @test s_0                        ≈ out.s_0
+    @test P_0                        ≈ out.P_0
 end
 
 # Method with initial conditions omitted
-out = kalman_filter(data, TTT, RRR, CCC, QQ, ZZ, DD, EE)
+out = kalman_filter(y, T, R, C, Q, Z, D, E)
 
 h5open("$path/reference/kalman_filter_out.h5", "r") do h5
     @test read(h5, "log_likelihood") ≈ sum(out.loglh)
@@ -31,8 +32,8 @@ h5open("$path/reference/kalman_filter_out.h5", "r") do h5
     @test read(h5, "vpred")          ≈ out.P_pred
     @test read(h5, "filt")           ≈ out.s_filt
     @test read(h5, "vfilt")          ≈ out.P_filt
-    @test z0                         ≈ out.s_0
-    @test P0                         ≈ out.P_0
+    @test s_0                        ≈ out.s_0
+    @test P_0                        ≈ out.P_0
 end
 
 
