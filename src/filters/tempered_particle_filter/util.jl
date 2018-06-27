a114e42e90d08fff466f74452fe72b829454d617
Pearl Li <pearlzli16@gmail.com>
2018-06-27 18:07:01 -0400

    Cosmetic changes to util.jl

--- src/filters/tempered_particle_filter/util.jl
+++ src/filters/tempered_particle_filter/util.jl
@@ -2,23 +2,26 @@ macro mypar(parallel, ex)
     return :( $(esc(parallel)) ? (@sync @parallel $(esc(ex))) : $(esc(ex)) )
 end
 
-function bisection(f::Function, a::Number, b::Number; xtol::AbstractFloat=1e-1, maxiter::Integer=1000)
+function bisection(f::Function, a::Number, b::Number;
+                   xtol::AbstractFloat = 1e-1, maxiter::Int = 1000)
     fa = f(a)
-    fa * f(b) <= 0 || throw("No real root in [a,b]")
+    sign(fa) == sign(f(b)) && throw("No real root in [a, b]")
     i = 0
     c = 0.0
     while b-a > xtol
         i += 1
-        i != maxiter || throw("Max iteration exceeded")
+        i == maxiter && throw("Max number of iterations exceeded")
         c = (a+b)/2
         fc = f(c)
         if fc ≈ 0
             break
         elseif sign(fa) == sign(fc)
+            # Root is in the right half of [a, b]
             a = c
-            fa = fc # Root is in the right half of [a,b]
+            fa = fc
         else
-            b = c # Root is in the left half of [a,b]
+            # Root is in the left half of [a, b]
+            b = c
         end
     end
     return c
@@ -30,8 +33,8 @@ function fast_mvnormal_pdf(x::Vector{Float64})
     return coeff_term*exp_term
 end
 
-function fast_mvnormal_pdf(x::Vector{Float64}, detΣ::Float64, invΣ::Matrix{Float64})
-    coeff_term = (2*pi)^(-length(x)/2) * detΣ^(-1/2)
-    exp_term   = exp(-(1/2) * dot(x, invΣ * x))
+function fast_mvnormal_pdf(x::Vector{Float64}, det_Σ::Float64, inv_Σ::Matrix{Float64})
+    coeff_term = (2*pi)^(-length(x)/2) * det_Σ^(-1/2)
+    exp_term   = exp(-(1/2) * dot(x, inv_Σ * x))
     return coeff_term*exp_term
 end
