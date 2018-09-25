36364315d5b58f81b66bfdfead109fb9584b5697
Ethan Matlin <ethanmatlin@gmail.com>
2018-09-25 14:42:59 -0400

    Change parallel test because @distributed seeds differently than @parallel

--- test/tempered_particle_filter.jl
+++ test/tempered_particle_filter.jl
@@ -100,11 +100,12 @@ mutation!(Φ, Ψ, QQ, det(HH), inv(HH), φ_new, data[:,47], s_t_nontemp, s_t1_te
 end
 
 ## Whole TPF Tests
-srand(47)
+Random.seed!(47)
 out_no_parallel = tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init; tuning..., verbose = :none, parallel = false)
-srand(47)
+Random.seed!(47)
 out_parallel_one_worker = tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init; tuning..., verbose = :none, parallel = true)
 @testset "TPF tests" begin
     @test out_no_parallel[1] ≈ -302.99967306704133
-    @test out_parallel_one_worker[1] ≈ -306.8211172094595
+    # This is different than in Julia6 because @distributed seeds differently than @parallel
+    @test out_parallel_one_worker[1] ≈ -303.64727963725073 #Julia 6 was: -306.8211172094595
 end
