f5eee19c9bdf23152d2fd84b07f5b4f0d45b6983
William Chen <wyc1@williams.edu>
2019-07-18 10:55:05 -0400

    Run TPF tests successfully with corrections to TPF

--- test/poolmodel_tpf.jl
+++ test/poolmodel_tpf.jl
@@ -1,29 +1,27 @@
-using Statistics, DSGE, DSGEModels, CSV, Dates
+using DSGE, DSGEModels, CSV, Dates
 
-# Read in from JLD
+# Set up and read in from JLD
 m805 = Model805()
 m904 = Model904()
 # The following stuff should be saved into tpf_main_input
-lppd_805 = load("reference/m805_preddens/logscores_T0=1991-12-31_T=2016-12-31_cond=semi_data=1_est=2_hor=4_samp=SMC.jld2")["logscores"]
-# lppd_805_4 = load("reference/m805_preddens/logscores_T0=1991-12-31_T=2016-12-31_cond=semi_data=4_est=2_hor=4_samp=SMC.jld2")["logscores"]
-lppd_904 = load("reference/m904_preddens/logscores_T0=1991-12-31_T=2016-12-31_cond=semi_data=1_est=2_hor=4_samp=SMC.jld2")["logscores"]
-# lppd_904_4 = load("reference/m904_preddens/logscores_T0=1991-12-31_T=2016-12-31_cond=semi_data=4_est=2_hor=4_samp=SMC.jld2")["logscores"]
-lppd_805 = vec(mean(lppd_805, dims = 1))
-# lppd_805_4 = vec(mean(lppd_805_4, dims = 1))
-lppd_904 = vec(mean(lppd_904, dims = 1))
-# lppd_904_4 = vec(mean(lppd_904_4, dims = 1))
-y805 = CSV.read("reference/realtime_spec=m805_hp=true_vint=170410.csv")
+# lpd_805 = load("reference/m805_preddens/logscores_T0=1991-12-31_T=2016-12-31_cond=semi_data=1_est=2_hor=4_samp=SMC.jld2")["logscores"]
+# lpd_904 = load("reference/m904_preddens/logscores_T0=1991-12-31_T=2016-12-31_cond=semi_data=1_est=2_hor=4_samp=SMC.jld2")["logscores"]
+# lpd_805 = vec(mean(lpd_805, dims = 1))
+# lpd_904 = vec(mean(lpd_904, dims = 1))
+
+y805 = CSV.read("reference/realtime_spec=m805_hp=true_vint=170410.csv") # is this data confidential???
 datevec = Vector{Date}(y805[y805.date .>= Date("1991-12-31"),1])
 y805 = Matrix{Float64}(Matrix(y805[y805.date .>= Date("1991-12-31"),:])[:,2:end]')
 y904 = CSV.read("reference/realtime_spec=m904_hp=true_vint=170410.csv")
 y904 = Matrix{Float64}(Matrix(y904[y904.date .>= Date("1991-12-31"),:])[:,2:end]')
-m = PoolModel(Dict(:Model805 => y805, :Model904 => y904), 4,
-               Dict(:Model805 => lppd_805, :Model904 => lppd_904),
-               [m805, m904])
-
 tpf_main_input = load("reference/tpf_poolmodel.jld2")
-data     = tpf_main_input["data"]
+data = tpf_main_input["data"]
 tuning = tpf_main_input["tuning"]
+lpd_805 = tpf_main_input["lpd_805"]
+lpd_904 = tpf_main_input["lpd_904"]
+m = PoolModel(Dict(:Model805 => y805, :Model904 => y904), 4,
+               Dict(:Model805 => exp.(lpd_805), :Model904 => exp.(lpd_904)),
+               [m805, m904])
 
 # Define transition eq, measurement eq, and shock distributions
 Ψ47_pm(x) = get_Ψ(m)(x,47)
@@ -43,7 +41,9 @@ HH = ones(1,1)
 
 s_t_nontemp = [.5; .5] * ones(1,1000) # this might be desirable since all particles same then
 Random.seed!(1793)
-weight_kernel!(coeff_terms, log_e_1_terms, log_e_2_terms, φ_old, Ψ47_pm, data[:, 47], s_t_nontemp, det(HH), inv(HH); initialize = false, parallel = false,
+weight_kernel!(coeff_terms, log_e_1_terms, log_e_2_terms, φ_old, Ψ47_pm, data[:, 47],
+               s_t_nontemp, det(HH), inv(HH);
+               initialize = false, parallel = false,
                dynamic_measurement = true, poolmodel = true)
 φ_new = next_φ(φ_old, coeff_terms, log_e_1_terms, log_e_2_terms, length(data[:,47]), tuning[:r_star], 2)
 correction!(inc_weights, norm_weights, φ_new, coeff_terms, log_e_1_terms, log_e_2_terms, length(data[:,47]))
@@ -51,8 +51,8 @@ true_inc_wt = φ_new^(1/2) * coeff_terms[1] * exp(log_e_1_terms[1]) * exp(φ_new
 
 @testset "Corection and Auxiliary Tests" begin
     @test coeff_terms[1] ≈ (φ_old)^(-1/2)
-    @test log_e_1_terms[1] ≈ get_Ψ(m)(s_t_nontemp[:,1], 47) * -φ_old
-    @test log_e_2_terms[1] ≈ get_Ψ(m)(s_t_nontemp[:,1], 47)
+    @test log_e_1_terms[1] ≈ log(get_Ψ(m)(s_t_nontemp[:,1], 47)) * -φ_old
+    @test log_e_2_terms[1] ≈ log(get_Ψ(m)(s_t_nontemp[:,1], 47))
     @test φ_new ≈ 1.0
     @test inc_weights[1] ≈ true_inc_wt
 end
@@ -65,7 +65,9 @@ log_e_2_terms = test_file_inputs["log_e_2_terms"]
 inc_weights = test_file_inputs["inc_weights"]
 s_t_nontemp0 = [.5; .5] * ones(1, 999)
 s_t_nontemp = hcat(s_t_nontemp0, [.49; .51])
-weight_kernel!(coeff_terms, log_e_1_terms, log_e_2_terms, φ_old, Ψ47_pm, data[:, 47], s_t_nontemp, det(HH), inv(HH); initialize = false, parallel = false,
+weight_kernel!(coeff_terms, log_e_1_terms, log_e_2_terms, φ_old, Ψ47_pm, data[:, 47],
+               s_t_nontemp, det(HH), inv(HH);
+               initialize = false, parallel = false,
                dynamic_measurement = true, poolmodel = true)
 φ_new = next_φ(φ_old, coeff_terms, log_e_1_terms, log_e_2_terms, length(data[:,47]), tuning[:r_star], 2)
 correction!(inc_weights, norm_weights, φ_new, coeff_terms, log_e_1_terms, log_e_2_terms, length(data[:,47]))
@@ -91,22 +93,28 @@ c = test_file_inputs["c"]
 
 c = update_c(c, accept_rate, tuning[:target_accept_rate])
 Random.seed!(47)
-StateSpaceRoutines.mutation!(get_Φ(m), Ψ47_pm, QQ, det(HH), inv(HH), φ_new, data[:,47], s_t_nontemp, s_t1_temp, ϵ_t, c, tuning[:n_mh_steps]; dynamic_measurement = true, poolmodel = true)
+StateSpaceRoutines.mutation!(get_Φ(m), Ψ47_pm, QQ, det(HH), inv(HH), φ_new, data[:,47],
+                             s_t_nontemp, s_t1_temp, ϵ_t, c, tuning[:n_mh_steps];
+                             dynamic_measurement = true, poolmodel = true)
 @testset "Mutation Tests" begin
-    @test s_t_nontemp[1] ≈ test_file_outputs_pm["s_t_nontemp"][1] # 0.2699446459556935 #
-    @test ϵ_t[1] ≈ test_file_outputs_pm["eps_t"][1] # -0.693335270423147625246545
+    @test s_t_nontemp[1] ≈ test_file_outputs_pm["s_t_nontemp"][1]
+    @test ϵ_t[1] ≈ test_file_outputs_pm["eps_t"][1]
 end
 
 ## Whole TPF Tests
 Random.seed!(47)
 s_init = reshape(rand(Uniform(.4, .6), 1000), 1, 1000)
 s_init = [s_init; 1 .- s_init]
-out_no_parallel = tempered_particle_filter(data, get_Φ(m), get_Ψ(m), get_F_ϵ(m), get_F_u(m), s_init; tuning..., verbose = :none,fixed_sched = [1.], parallel = false, dynamic_measurement = true, poolmodel = true)
+out_no_parallel = tempered_particle_filter(data, get_Φ(m), get_Ψ(m), get_F_ϵ(m), get_F_u(m), s_init;
+                                           tuning..., verbose = :none, fixed_sched = [1.],
+                                           parallel = false, dynamic_measurement = true, poolmodel = true)
 Random.seed!(47)
-out_parallel_one_worker = tempered_particle_filter(data, get_Φ(m), get_Ψ(m), get_F_ϵ(m), get_F_u(m), s_init; tuning..., verbose = :none, fixed_sched = [1.], parallel = true, dynamic_measurement = true, poolmodel = true)
+out_parallel_one_worker = tempered_particle_filter(data, get_Φ(m), get_Ψ(m), get_F_ϵ(m), get_F_u(m), s_init;
+                                                   tuning..., verbose = :none, fixed_sched = [1.],
+                                                   parallel = true, dynamic_measurement = true, poolmodel = true)
 @testset "TPF tests" begin
-    @test out_no_parallel[1] ≈ test_file_outputs_pm["out_no_parallel"][1] # -423.17791042050027
-    @test out_parallel_one_worker[1] ≈ test_file_outputs_pm["out_parallel_one_worker"][1] # -423.2233360059381
+    @test out_no_parallel[1] ≈ test_file_outputs_pm["out_no_parallel"][1]
+    @test out_parallel_one_worker[1] ≈ test_file_outputs_pm["out_parallel_one_worker"][1]
 end
 
 nothing
