aa30e62c7c2afdbe66d2a39e258062953adb9c98
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-12-11 10:43:03 -0500

    Clean test for dynamic measurement equation (trying to trigger a rebuild...)

--- test/dyn_measure_tpf.jl
+++ test/dyn_measure_tpf.jl
@@ -2,14 +2,14 @@ using JLD2
 
 # Read in from JLD
 tpf_main_input = load("reference/tpf_main_inputs.jld2")
-data = tpf_main_input["data"]
-TTT = tpf_main_input["TTT"]
-RRR = tpf_main_input["RRR"]
-CCC = tpf_main_input["CCC"]
-ZZ = tpf_main_input["ZZ"]
-DD = tpf_main_input["DD"]
-F_ϵ = tpf_main_input["F_epsilon"]
-F_u = tpf_main_input["F_u"]
+data   = tpf_main_input["data"]
+TTT    = tpf_main_input["TTT"]
+RRR    = tpf_main_input["RRR"]
+CCC    = tpf_main_input["CCC"]
+ZZ     = tpf_main_input["ZZ"]
+DD     = tpf_main_input["DD"]
+F_ϵ    = tpf_main_input["F_epsilon"]
+F_u    = tpf_main_input["F_u"]
 s_init = tpf_main_input["s_init"]
 
 # Tune algorithm
@@ -27,14 +27,14 @@ tuning = Dict(:r_star => 2., :c_init => 0.3, :target_accept_rate => 0.4,
 test_file_inputs = load("reference/tpf_aux_inputs.jld2")
 test_file_outputs = load("reference/tpf_aux_outputs.jld2")
 
-φ_old = test_file_inputs["phi_old"]
-norm_weights = test_file_inputs["norm_weights"]
-coeff_terms = test_file_inputs["coeff_terms"]
+φ_old         = test_file_inputs["phi_old"]
+norm_weights  = test_file_inputs["norm_weights"]
+coeff_terms   = test_file_inputs["coeff_terms"]
 log_e_1_terms = test_file_inputs["log_e_1_terms"]
 log_e_2_terms = test_file_inputs["log_e_2_terms"]
-inc_weights = test_file_inputs["inc_weights"]
-HH = cov(F_u)
-s_t_nontemp = test_file_inputs["s_t_nontemp"]
+inc_weights   = test_file_inputs["inc_weights"]
+s_t_nontemp   = test_file_inputs["s_t_nontemp"]
+HH            = cov(F_u)
 
 weight_kernel!(coeff_terms, log_e_1_terms, log_e_2_terms, φ_old, Ψ, data[:, 47], s_t_nontemp, det(HH), inv(HH);
                initialize = false, parallel = false)
@@ -43,25 +43,25 @@ weight_kernel!(coeff_terms, log_e_1_terms, log_e_2_terms, φ_old, Ψ, data[:, 47
 correction!(inc_weights, norm_weights, φ_new, coeff_terms, log_e_1_terms, log_e_2_terms, length(data[:,47]))
 
 @testset "Correction and Auxiliary Tests" begin
-    @test coeff_terms[1] ≈ test_file_outputs["coeff_terms"][1]
+    @test coeff_terms[1]   ≈ test_file_outputs["coeff_terms"][1]
     @test log_e_1_terms[1] ≈ test_file_outputs["log_e_1_terms"][1]
     @test log_e_2_terms[1] ≈ test_file_outputs["log_e_2_terms"][1]
-    @test φ_new ≈ test_file_outputs["phi_new"]
-    @test inc_weights[1] ≈ test_file_outputs["inc_weights"][1]
+    @test φ_new            ≈ test_file_outputs["phi_new"]
+    @test inc_weights[1]   ≈ test_file_outputs["inc_weights"][1]
 end
 
 # Incorrect measurement equation
 Ψtinc(s_t::AbstractVector{Float64}, t) = t < 46 ? ZZ*s_t + DD : ZZ*s_t + DD + [0.; 1; 0.];
 Ψinc(x) = Ψtinc(x, 47)
 
-φ_old = test_file_inputs["phi_old"]
-norm_weights = test_file_inputs["norm_weights"]
-coeff_terms = test_file_inputs["coeff_terms"]
+φ_old         = test_file_inputs["phi_old"]
+norm_weights  = test_file_inputs["norm_weights"]
+coeff_terms   = test_file_inputs["coeff_terms"]
 log_e_1_terms = test_file_inputs["log_e_1_terms"]
 log_e_2_terms = test_file_inputs["log_e_2_terms"]
-inc_weights = test_file_inputs["inc_weights"]
-HH = cov(F_u)
-s_t_nontemp = test_file_inputs["s_t_nontemp"]
+inc_weights   = test_file_inputs["inc_weights"]
+s_t_nontemp   = test_file_inputs["s_t_nontemp"]
+HH            = cov(F_u)
 
 weight_kernel!(coeff_terms, log_e_1_terms, log_e_2_terms, φ_old, Ψinc, data[:, 47], s_t_nontemp, det(HH), inv(HH);
                initialize = false, parallel = false)
@@ -71,8 +71,8 @@ correction!(inc_weights, norm_weights, φ_new, coeff_terms, log_e_1_terms, log_e
 @testset "Correction and Auxiliary Tests with Dynamically Wrong Measurement Equation" begin
     @test !(log_e_1_terms[1] ≈ test_file_outputs["log_e_1_terms"][1])
     @test !(log_e_2_terms[1] ≈ test_file_outputs["log_e_2_terms"][1])
-    @test !(φ_new ≈ test_file_outputs["phi_new"])
-    @test !(inc_weights[1] ≈ test_file_outputs["inc_weights"][1])
+    @test !(φ_new            ≈ test_file_outputs["phi_new"])
+    @test !(inc_weights[1]   ≈ test_file_outputs["inc_weights"][1])
 end
 
 ## Selection Tests
@@ -84,9 +84,9 @@ s_t1_temp = test_file_inputs["s_t1_temp"]
 Random.seed!(47)
 selection!(norm_weights, s_t1_temp, s_t_nontemp,ϵ_t, resampling_method = tuning[:resampling_method])
 @testset "Selection Tests" begin
-    @test s_t1_temp[1] ≈ test_file_outputs["s_t1_temp"][1]
+    @test s_t1_temp[1]   ≈ test_file_outputs["s_t1_temp"][1]
     @test s_t_nontemp[1] ≈ test_file_outputs["s_t_nontemp"][1]
-    @test ϵ_t[1] ≈ test_file_outputs["eps_t"][1]
+    @test ϵ_t[1]         ≈ test_file_outputs["eps_t"][1]
 end
 
 ## Mutation Tests
