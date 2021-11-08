1a3e414c59462be5d57cc8d7dce1a9db2736b816
Shlok Goyal <shlok.goyal@gmail.com>
2021-11-08 09:29:02 -0500

    Clean doc strings in EnKF

--- src/filters/EnKF.jl
+++ src/filters/EnKF.jl
@@ -1,11 +1,9 @@
 """
 ```
-tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init;
-    n_particles = 1000, fixed_sched = [], r_star = 2, findroot = bisection,
-    xtol = 1e-3, resampling_method = :multionial, n_mh_steps = 1, c_init = 0.3,
-    target_accept_rate = 0.4, n_presample_periods = 0, allout = true,
-    parallel = false, verbose = :none,
-    dynamic_measurement = false, poolmodel = false)
+ensemble_kalman_filter(data, Φ, Ψ, F_ϵ, F_u, s_init;
+    n_particles = 1000, n_presample_periods = 0,
+    allout = true, get_t_particle_dist::Bool = false,
+    parallel = false, verbose = :none)
 ```
 
 ### Inputs
@@ -28,30 +26,6 @@ where `S<:AbstractFloat` and
 - `n_particles::Int`: number of particles used to approximate the
   log-likelihood. Using more particles yields a more accurate estimate, at the
   cost of being more computationally intensive
-
-**Correction:**
-
-- `fixed_sched::Vector{S}`: array of monotone increasing values in (0,1] which
-  specify the fixed tempering schedule. Set to `[]` if you wish to adaptively
-  choose the tempering schedule (below)
-- `r_star::S`: target inefficiency ratio used to adaptively choose φ, i.e.
-  Ineff(φ_n) := 1/M Σ_{j=1}^M (Wtil_t^{j,n}(φ_n))^2 = r_star
-- `findroot::Function`: root-finding algorithm, one of `bisection` or `fzero`
-- `xtol::S`: root-finding tolerance on x bracket size
-
-**Selection:**
-
-- `resampling_method::Symbol`: either `:multinomial` or `:systematic`
-
-**Mutation:**
-
-- `n_mh_steps::Int`: number of Metropolis-Hastings steps to take in each
-  tempering stage
-- `c_init::S`: initial scaling of proposal covariance matrix
-- `target_accept_rate::S`: target Metropolis-Hastings acceptance rate
-
-**Other:**
-
 - `n_presample_periods::Int`: number of initial periods to omit from the
   log-likelihood calculation
 - `allout::Bool`: whether to return all outputs or just `sum(loglh)`
