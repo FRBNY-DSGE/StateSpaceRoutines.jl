a9d92641b8d92937323a74402c30ce18a6d08f7f
Shlok Goyal <shlok.goyal@gmail.com>
2022-01-20 16:26:27 -0500

    Test TPF with n_states, n_shocks = 1

--- test/parallel_tempered_particle_filter.jl
+++ test/parallel_tempered_particle_filter.jl
@@ -1,12 +1,16 @@
 @show "OK?"
-using JLD, JLD2, Test, Distributions, Random, StateSpaceRoutines, BenchmarkTools, ParallelDataTransfer, FLoops, DistributedArrays, OffsetArrays, DistributedArrays.SPMD
+using JLD, JLD2, Test, Distributions, Random, StateSpaceRoutines, BenchmarkTools, ParallelDataTransfer, DistributedArrays, OffsetArrays, DistributedArrays.SPMD
 
-addproc_num = 6 ## Set to 0 to not add workers
-nparts_mill = true
+addproc_num = 0 ## Set to 0 to not add workers
+nparts_mill = false
 nparts_mult = nparts_mill ? 10 : 1
 fixed_sched = [1.0]#[0.1,0.15,0.3,1.0]
+run_timing  = false
+only_tpf    = true
+n_states1   = false
+n_shocks1   = false
 
-@show fixed_sched
+@show fixed_sched, n_states1, n_shocks1
 @show "Parallel " * string(addproc_num)
 @show "No of particles (in thousands): " * string(nparts_mult)
 
@@ -22,6 +26,24 @@ F_ϵ = tpf_main_input["F_epsilon"]
 F_u = tpf_main_input["F_u"]
 s_init = tpf_main_input["s_init"]
 
+if n_states1
+    s_init = vec(s_init[1,:])
+    TTT = n_shocks1 ? 0.5 : TTT[1,1]
+    RRR = RRR[1,:]'
+    CCC = CCC[1]
+    ZZ = ZZ[:,1]
+
+    if n_shocks1
+        RRR = 0.9#RRR[1,1]
+        F_ϵ = Normal(F_ϵ.μ[1], F_ϵ.Σ[1,1])
+    end
+end
+
+if n_shocks1 && !n_states1
+    RRR = RRR[:,1]
+    F_ϵ = Normal(F_ϵ.μ[1], F_ϵ.Σ[1,1])
+end
+
 if nparts_mill
     s_init = repeat(s_init, outer=(1,nparts_mult))
 end
@@ -36,8 +58,15 @@ if fixed_sched != []
 end
 
 # Define Φ and Ψ (can't be saved to JLD)
-Φ(s_t::AbstractVector{Float64}, ϵ_t::AbstractVector{Float64}) = TTT*s_t + RRR*ϵ_t + CCC
-Ψ(s_t::AbstractVector{Float64}) = ZZ*s_t + DD
+if !n_states1 && n_shocks1
+    Φ(s_t::AbstractVector{Float64}, ϵ_t::AbstractVector{Float64}) = TTT*s_t .+ RRR .* ϵ_t .+ CCC
+else
+    Φ(s_t::AbstractVector{Float64}, ϵ_t::AbstractVector{Float64}) = TTT*s_t + RRR*ϵ_t + CCC
+end
+Ψ(s_t::AbstractVector{Float64}) = ZZ .* s_t .+ DD
+
+Φ(s_t::Float64, ϵ_t::Float64) = TTT*s_t + RRR*ϵ_t + CCC
+Ψ(s_t::Float64) = ZZ*s_t + DD
 
 # Load in test inputs and outputs
 test_file_inputs = load("reference/tpf_aux_inputs.jld2")
@@ -51,6 +80,10 @@ log_e_2_terms = test_file_inputs["log_e_2_terms"]
 inc_weights = test_file_inputs["inc_weights"]
 HH = cov(F_u)
 s_t_nontemp = test_file_inputs["s_t_nontemp"]
+
+if n_states1
+    s_t_nontemp = s_t_nontemp[1,:]#reshape(s_t_nontemp[1,:], (1,size(s_t_nontemp,2)))
+end
 
 if nparts_mill
     s_t_nontemp = repeat(s_t_nontemp, outer=(1,nparts_mult))
@@ -73,9 +106,47 @@ end
 @everywhere CCC = tpf_main_input["CCC"]
 @everywhere ZZ = tpf_main_input["ZZ"]
 @everywhere DD = tpf_main_input["DD"]
+
 @everywhere Φ(s_t::AbstractVector{Float64}, ϵ_t::AbstractVector{Float64}) = TTT*s_t + RRR*ϵ_t + CCC
 @everywhere Ψ(s_t::AbstractVector{Float64}) = ZZ*s_t + DD
+@everywhere Φ(s_t::Float64, ϵ_t::Float64) = TTT*s_t + RRR*ϵ_t + CCC
+@everywhere Ψ(s_t::Float64) = ZZ*s_t + DD
 
+if n_states1
+    if n_shocks1
+        @everywhere TTT = 0.5
+        @everywhere RRR = 0.9
+        @everywhere ZZ = ZZ[:,1]
+    else
+        @everywhere TTT = TTT[1,1]
+        @everywhere RRR = RRR[1,:]'
+        @everywhere ZZ = ZZ[:,1]
+    end
+    @everywhere CCC = CCC[1]
+
+    #=if n_shocks1
+        @everywhere F_ϵ = Normal(F_ϵ.μ[1], F_ϵ.Σ[1,1])
+    end=#
+end
+
+if n_shocks1 && !n_states1
+    @everywhere RRR = RRR[:,1]
+    # @everywhere F_ϵ = Normal(F_ϵ.μ[1], F_ϵ.Σ[1,1])
+end
+
+if n_shocks1 || n_states1
+    if !n_states1
+        @everywhere Φ(s_t::AbstractVector{Float64}, ϵ_t::AbstractVector{Float64}) = TTT*s_t .+ RRR .* ϵ_t .+ CCC
+        @everywhere Ψ(s_t::AbstractVector{Float64}) = ZZ*s_t + DD
+    else
+        @everywhere Φ(s_t::AbstractVector{Float64}, ϵ_t::AbstractVector{Float64}) = TTT*s_t .+ RRR*ϵ_t .+ CCC
+        @everywhere Ψ(s_t::AbstractVector{Float64}) = ZZ .* s_t .+ DD
+    end
+    @everywhere Φ(s_t::Float64, ϵ_t::Float64) = TTT*s_t + RRR*ϵ_t + CCC
+    @everywhere Ψ(s_t::Float64) = ZZ*s_t + DD
+end
+
+if !only_tpf
 #= StateSpaceRoutines.sendto(workers(), Φ = Φ)
 StateSpaceRoutines.sendto(workers(), Ψ = Ψ)
 StateSpaceRoutines.sendto(workers(), coeff_terms = coeff_terms)
@@ -157,51 +228,105 @@ s_t1_temp_vec = convert(Matrix, s_t1_temp)
 end
 
 ## Mutation Tests
-QQ = cov(F_ϵ)
+QQ = n_shocks1 ? var(F_ϵ) : cov(F_ϵ)
 accept_rate = test_file_inputs["accept_rate"]
 c = test_file_inputs["c"]
 
 c = update_c(c, accept_rate, tuning[:target_accept_rate])
-Random.seed!(47)
-#= # No mutation in BSPF, although I will implement this later.
-@btime StateSpaceRoutines.mutation!(Φ, Ψ, QQ, det(HH), inv(HH), φ_new, data[:,47], s_t_nontemp_vec, s_t1_temp_vec, ϵ_t_vec, c, tuning[:n_mh_steps], parallel = false)
-@btime spmd(StateSpaceRoutines.mutation!, Φ, Ψ, QQ, det(HH), inv(HH), φ_new, data[:,47], s_t_nontemp, s_t1_temp, ϵ_t, c, tuning[:n_mh_steps])
 
-@testset "Mutation Tests" begin
-    @test s_t_nontemp[1] ≈ test_file_outputs["s_t_nontemp_mutation"][1]
-    @test ϵ_t[1] ≈ test_file_outputs["eps_t_mutation"][1]
+#@assert false
+
+# No mutation in BSPF, although I will implement this later.
+#=if run_timing
+    @show "Sequential mutation"
+    Random.seed!(47)
+    @everywhere Random.seed!(47)
+    @btime StateSpaceRoutines.mutation!($Φ, $Ψ, $QQ, det($HH), inv($HH), $φ_new, $data[:,47], $s_t_nontemp_vec, $s_t1_temp_vec, $ϵ_t_vec, $c, $tuning[:n_mh_steps])
+    @show "Parallel mutation"
+    Random.seed!(47)
+    @everywhere Random.seed!(47)
+    @btime spmd(StateSpaceRoutines.mutation!, $Φ, $Ψ, $QQ, det($HH), inv($HH), $φ_new, $data[:,47], $s_t_nontemp, $s_t1_temp, $ϵ_t, $c, $tuning[:n_mh_steps])
 end
 =#
+# @assert false
+
+Random.seed!(47)
+@everywhere Random.seed!(47)
+StateSpaceRoutines.mutation!(Φ, Ψ, QQ, det(HH), inv(HH), φ_new, data[:,47], s_t_nontemp_vec, s_t1_temp_vec, ϵ_t_vec, c, tuning[:n_mh_steps])
+@everywhere Random.seed!(47)
+Random.seed!(47)
+spmd(StateSpaceRoutines.mutation!, Φ, Ψ, QQ, det(HH), inv(HH), φ_new, data[:,47], s_t_nontemp, s_t1_temp, ϵ_t, c, tuning[:n_mh_steps])
+@testset "Mutation Tests" begin
+    @test convert(Matrix, s_t_nontemp)[1] ≈ s_t_nontemp_vec[1]#test_file_outputs["s_t_nontemp_mutation"][1]
+    @test convert(Matrix, ϵ_t)[1] ≈ ϵ_t_vec[1]#test_file_outputs["eps_t_mutation"][1]
+end
+end
 # @btime StateSpaceRoutines.mutation!(Φ, Ψ, QQ, det(HH), inv(HH), φ_new, data[:,47], s_t_nontemp, s_t1_temp, ϵ_t, c,
 #                                     tuning[:n_mh_steps], parallel = true)
 # @btime out_parallel_one_worker = tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init; tuning..., verbose = :none, parallel = true)
 
 ## Whole TPF Tests
-Random.seed!(47)
-out_no_parallel = tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init; tuning..., verbose = :none, parallel = false)
-Random.seed!(47)
-out_parallel_one_worker = tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init; tuning..., verbose = :none, parallel = true)
 
-@btime out_no_parallel = tempered_particle_filter($data, $Φ, $Ψ, $F_ϵ, $F_u, $s_init; $tuning..., verbose = :none, parallel = false)
-@btime out_parallel_one_worker = tempered_particle_filter($data, $Φ, $Ψ, $F_ϵ, $F_u, $s_init; $tuning..., verbose = :none, parallel = true)
+# BSPF Test
+@everywhere seed_val = 47
+
+tuning[:fixed_sched] = [1.0]
+@everywhere Random.seed!(seed_val)
+out_no_parallel = tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init; tuning..., verbose = :none, parallel = false, parallel_testing = true)
+@everywhere Random.seed!(seed_val)
+out_parallel_one_worker = tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init; tuning..., verbose = :none, parallel = true, parallel_testing = true)
+
+if run_timing
+    @show "BSPF in sequential"
+    @btime out_no_parallel = tempered_particle_filter($data, $Φ, $Ψ, $F_ϵ, $F_u, $s_init; $tuning..., verbose = :none, parallel = false)
+    @show "BSPF in parallel"
+    @btime out_parallel_one_worker = tempered_particle_filter($data, $Φ, $Ψ, $F_ϵ, $F_u, $s_init; $tuning..., verbose = :none, parallel = true)
+end
 @show out_no_parallel[1], out_parallel_one_worker[1]
+
+# Adaptive TPF Test
+if !n_shocks1 # Bisection doesn't have real roots in this case
+    delete!(tuning, :fixed_sched)
+    @everywhere Random.seed!(seed_val)
+    adapt_no_parallel = tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init; tuning..., verbose = :none, parallel = false, parallel_testing = true)
+    @everywhere Random.seed!(seed_val)
+    adapt_parallel_one_worker = tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init; tuning..., verbose = :none, parallel = true, parallel_testing = true)
+
+if run_timing
+    @show "Adaptive TPF in sequential"
+    @btime adapt_no_parallel = tempered_particle_filter($data, $Φ, $Ψ, $F_ϵ, $F_u, $s_init; $tuning..., verbose = :none, parallel = false)
+    @show "Adaptive TPF in parallel"
+    @btime adapt_parallel_one_worker = tempered_particle_filter($data, $Φ, $Ψ, $F_ϵ, $F_u, $s_init; $tuning..., verbose = :none, parallel = true)
+end
+@show adapt_no_parallel[1], adapt_parallel_one_worker[1]
+end
+
+# Fixed Schedule TPF Test
+tuning[:fixed_sched] = [0.1,0.15,0.3,1.0]
+@everywhere Random.seed!(seed_val)
+fixed_no_parallel = tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init; tuning..., verbose = :none, parallel = false, parallel_testing = true)
+@everywhere Random.seed!(seed_val)
+fixed_parallel_one_worker = tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init; tuning..., verbose = :none, parallel = true, parallel_testing = true)
+
+if run_timing
+    @show "Fixed schedule TPF in sequential"
+    @btime fixed_no_parallel = tempered_particle_filter($data, $Φ, $Ψ, $F_ϵ, $F_u, $s_init; $tuning..., verbose = :none, parallel = false)
+    @show "Fixed schedule TPF in parallel"
+    @btime fixed_parallel_one_worker = tempered_particle_filter($data, $Φ, $Ψ, $F_ϵ, $F_u, $s_init; $tuning..., verbose = :none, parallel = true)
+end
+@show fixed_no_parallel[1], fixed_parallel_one_worker[1]
+
 @testset "TPF tests" begin
-    # @test out_no_parallel[1] ≈ -302.99967306704133 ## Not applicable with parallel in DArray
-    # @test out_parallel_one_worker[1] ≈ -302.99967306704133
     if addproc_num <= 1
-        @test all(out_no_parallel .== out_parallel_one_worker)
+        @test all(out_no_parallel[2] .≈ out_parallel_one_worker[2])
+        @test (n_shocks1) || all(adapt_no_parallel[2] .≈ adapt_parallel_one_worker[2])
+        @test all(fixed_no_parallel[2] .≈ fixed_parallel_one_worker[2])
     else
         @test abs(out_no_parallel[1] - out_parallel_one_worker[1]) ≤ 10.0
+        @test (n_shocks1) || abs(adapt_no_parallel[1] - adapt_parallel_one_worker[1]) ≤ 10.0
+        @test abs(fixed_no_parallel[1] - fixed_parallel_one_worker[1]) ≤ 10.0
     end
-    ## Note when using more than 1 worker, this is not true because there is still a random step in mh_steps
-#=
-    # In Julia 1.5, seeding appears to be different
-    if VERSION >=  v"1.5"
-        @test out_parallel_one_worker[1] ≈ -307.9285106003482
-    elseif VERSION >= v"1.0" # This is different than in Julia6 because @distributed seeds differently than @parallel
-        @test out_parallel_one_worker[1] ≈ -303.64727963725073 # Julia 6 was: -306.8211172094595
-    end
-=#
+    ## Note when using more than 1 worker, equality is not true because there is still a random step in mh_steps
 end
 
 for i in workers()
