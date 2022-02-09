a7d8428bdab1ad2a55beaadff31d60fc8bb13ce3
Shlok Goyal <shlok.goyal@gmail.com>
2022-02-09 10:10:21 -0500

    Update example b/c readtable deprecated

--- docs/examples/tempered_particle_filter/tempered_particle_filter_ex.jl
+++ docs/examples/tempered_particle_filter/tempered_particle_filter_ex.jl
@@ -1,16 +1,17 @@
 using DSGE, StateSpaceRoutines
 using QuantEcon: solve_discrete_lyapunov
-using DataFrames
+using DataFrames, CSV
 
 # Setup the model and data
 
 m = AnSchorfheide()
-df = readtable("us.txt", header = false, separator = ' ')
+#df = readtable("us.txt", header = false, separator = ' ')
+df = CSV.read("us.txt", DataFrame; delim=' ', ignorerepeated = true, type=Float64, header = false)
 data = convert(Matrix{Float64}, df)'
 
 params = [2.09, 0.98, 2.25, 0.65, 0.34, 3.16, 0.51, 0.81, 0.98, 0.93, 0.19, 0.65, 0.24,
           0.115985, 0.294166, 0.447587]
-update!(m, params)
+DSGE.update!(m, params)
 
 # Solution to a Linear DSGE Model w/ IID Gaussian Errors
 
